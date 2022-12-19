0184b16647cef6573e89e7242ac16878be69c4a5
Melli1234d <58852401+Melli1234d@users.noreply.github.com>
2022-12-19 22:51:22 +0100

    Firebase anpassen

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import {getFirestore} from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAXKcBEUQ-Ey9rqaRon8VQiyjdV5YawZDQ",
-    authDomain: "move-me-webapp.firebaseapp.com",
-    projectId: "move-me-webapp",
-    storageBucket: "move-me-webapp.appspot.com",
-    messagingSenderId: "1087452600725",
-    appId: "1:1087452600725:web:09b64ad9da5aa8c5397e9c",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 
