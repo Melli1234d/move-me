2d8706149e38786a9b8c54bfa01a92a8f530a18f
Melli1234d <58852401+Melli1234d@users.noreply.github.com>
2023-01-28 14:35:06 +0100

    kalender

--- README.md
+++ README.md
@@ -88,6 +88,7 @@ Code + CSS:
 14. Code Photo nach Firebase hochladen: or github: https://github.com/machadop1407/firebase-file-upload
 15. https://reactjs.org/docs/context.html
 16. https://css-tricks.com/using-requestanimationframe-with-react-hooks/
+17. https://codepen.io/eliza-rjb/pen/xmbEWX
 
 
 Bilder & Icons:
--- src/Pages/Zeitplan/Zeitplan.js
+++ src/Pages/Zeitplan/Zeitplan.js
@@ -6,9 +6,11 @@ import {collection, onSnapshot} from "firebase/firestore";
 import {firestore} from "../../firebase";
 import ListElement from "../../components/UI/ListElement";
 import RoundButton from "../../components/UI/RoundButton";
-import Einkaufsmoeglichkeit from "../../components/Pictures/wagen2.png";
-import Kalender from "../../components/Pictures/Zeitplan/calendar-event.svg";
-
+import KalenderIcon from "../../components/Pictures/Zeitplan/calendar-event.svg";
+import Zerbrechlich from "../../components/Pictures/MoebelAngaben/zerbrechlich.png";
+import Verpackung from "../../components/Pictures/MoebelAngaben/verpckung.png";
+import Kratzer from "../../components/Pictures/MoebelAngaben/kratzer.png";
+import Kalender from "../../components/Kalender/Kalender";
 //code: https://github.com/samfromaway/firebase-tutorial/blob/master/src/SnapshotFirebaseAdvanced.js
 
 
@@ -102,7 +104,50 @@ const Zeitplan = (props) => {
         setView('subtasks');
     }
 
+    function getLengthofListElement(task) {//alle label und das einzelne als wert mitgegeben
+       if(task.length === 1) {
+           return <ListElement key={task.id} onClick={handleTasks} className="listElement-one">
+               <div className="timeplaner">
+                   <p>{task.day} {task.month} {task.year}</p>
+                   <h5>{task.title}</h5>
+               </div>
 
+           </ListElement>;
+       } else if(task.length === 2) {
+           return <ListElement key={task.id} onClick={handleTasks} className="listElement-two">
+               <div className="timeplaner">
+                   <p>{task.day} {task.month} {task.year}</p>
+                   <h5>{task.title}</h5>
+               </div>
+
+           </ListElement>;
+       }if(task.length === 3) {
+            return <ListElement key={task.id} onClick={handleTasks} className="listElement-tree">
+                <div className="timeplaner">
+                    <p>{task.day} {task.month} {task.year}</p>
+                    <h5>{task.title}</h5>
+                </div>
+
+            </ListElement>;
+        } if(task.length === 4) {
+            return <ListElement key={task.id} onClick={handleTasks} className="listElement-four">
+                <div className="timeplaner">
+                    <p>{task.day} {task.month} {task.year}</p>
+                    <h5>{task.title}</h5>
+                </div>
+
+            </ListElement>;
+        }
+        if(task.length === 5) {
+            return <ListElement onClick={handleTasks} className="listElement-five">
+                <div className="timeplaner">
+                    <p>{task.day} {task.month} {task.year}</p>
+                    <h5>{task.title}</h5>
+                </div>
+
+            </ListElement>;
+        }
+    }
 
 
 
@@ -116,7 +161,7 @@ const Zeitplan = (props) => {
                 <h2> Zeitplan</h2>
                 <button onClick={handleClick} id="kalender-icon">
                     <div className="icon-calender">
-                        <RoundButton id="Kalender-icon" ><img id="calendar" src={Kalender} alt="Kalender Icon" height={18} width={18} /></RoundButton>
+                        <RoundButton id="Kalender-icon" ><img id="calendar" src={KalenderIcon} alt="Kalender Icon" height={18} width={18} /></RoundButton>
                     </div>
                 </button>
 
@@ -128,13 +173,7 @@ const Zeitplan = (props) => {
                 <>
                     <div className="timeplan-grid">
                         {tasks.map((task) => (
-                            <ListElement key={task.id} onClick={handleTasks}>
-                                <div className="timeplaner">
-                                    <p>{task.day} {task.month} {task.year}</p>
-                                    <h5>{task.title}</h5>
-                                </div>
-
-                            </ListElement>
+                            <div key={task.id}>{getLengthofListElement(task)}</div>
                         ))}
                     </div>
                 </>
@@ -144,13 +183,14 @@ const Zeitplan = (props) => {
             {view === 'calendar' &&
 
                 <>
-                    {subtasks.map((subtask) => (
-                        <ListElement key={subtask.id}>
+                    <Kalender/>
+                    {appointments.map((appointment) => (
+                        <div key={appointment.id}>
                             <div className="timeplaner">
-                                <p>{subtask.title} </p>
+                                <p>{appointment.title} </p>
                             </div>
 
-                        </ListElement>
+                        </div>
                     ))}
                 </>
             }
@@ -159,10 +199,10 @@ const Zeitplan = (props) => {
 
             {view === 'subtasks' &&
                 <>
-                    {appointments.map((appointment) => (
-                        <div key={appointment.id}>
+                    {subtasks.map((subtask) => (
+                        <div key={subtask.id}>
                             <div className="timeplaner">
-                                <p>{appointment.title} </p>
+                                <p>{subtask.title} </p>
                             </div>
 
                         </div>
