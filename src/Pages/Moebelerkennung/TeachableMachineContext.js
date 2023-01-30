f1791f60ba5e6f4977af9e402278bd3d99daa645
Melli1234d <58852401+Melli1234d@users.noreply.github.com>
2023-01-30 23:51:08 +0100

    Kommentare hinzufügen

--- src/Pages/Moebelerkennung/TeachableMachineContext.js
+++ src/Pages/Moebelerkennung/TeachableMachineContext.js
@@ -1,44 +1,38 @@
-
 import * as tmImage from "@teachablemachine/image";
 import React from 'react';
-const URL = 'tm-my-image-model-6/';
-//https://reactjs.org/docs/context.html
-//Code angelehnt an Code Beispiel der automatisch von Teachable Machine generiert wird
 
+
+//#############################################################################################################################################################
+//CODE: https://reactjs.org/docs/context.html
+//CODE:  angelehnt an Code Beispiel der automatisch von Teachable Machine generiert wird
+//#############################################################################################################################################################
+
+
+const URL = 'tm-my-image-model-6/'; //definieren des Pfades von dem Teachable Machine Modell
 const TeachableMachineWrapper = {
+//#############################################################################################################################################################
+// KAMERA STARTEN & MODEL DEFINIEREN
+//#############################################################################################################################################################
     async start(){
         const modelURL = URL + "model.json";
         const metadataURL = URL + "metadata.json";
-
-        // load the model and metadata
-        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-        // or files from your local hard drive
-        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-        const model = await tmImage.load(modelURL, metadataURL);
-
-
-
-        // Convenience function to setup a webcam
-        const flip = true; // whether to flip the webcam
-        const webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-        await webcam.setup({ facingMode: "environment" }); // request access to the webcam
-
-
+        const model = await tmImage.load(modelURL, metadataURL); //dem model wird die url und die metadaten mit gegeben
+        const flip = true; // kamera umdrehen
+        const webcam = new tmImage.Webcam(200, 200, flip); // neue Camera Webcam erstellen mit breite, höhe und gefliptem Bild im Container
+        await webcam.setup({ facingMode: "environment" }); // Zugang zur Kamera anfragen
         await webcam.play(); //webcam geht an
-
         this.model = model;
-
         this.webcam = webcam;
         this.started = true; //wird gestartet
     },
-//funktioniert noch nicht
+//#############################################################################################################################################################
+// KAMERA STOPPEN
+//#############################################################################################################################################################
     async stop(){
-        await this.webcam.stop() //vorherige webcam ansprechen
-        this.started = false; //wird gestoppt
-
+        await this.webcam.stop() // vorherige definierte webcam ansprechen & stoppen
+        this.started = false; // wird gestoppt
     }
-
 }
 
 // eslint-disable-next-line no-undef
-export const TeachableMachineContext = React.createContext(TeachableMachineWrapper)
+export const TeachableMachineContext = React.createContext(TeachableMachineWrapper) //Context erstellen
